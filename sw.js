1d966391e3b6767a4a505777290c6102c28506eb
SHASHI <135663528+ShashiDao@users.noreply.github.com>
2026-04-03 01:15:42 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// This file allows your site to be installed as an app
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // Logic for offline support can go here later
-});
