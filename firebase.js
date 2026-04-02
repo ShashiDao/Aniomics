ecdaf9443b9bae3e7a271d0ff6543570702a2f72
SHASHI <135663528+ShashiDao@users.noreply.github.com>
2026-04-02 23:09:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth, signInAnonymously } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// --- THE SACRED CONNECTION ---
 const firebaseConfig = {
   apiKey: "AIzaSyBlPdWgHqNAfxQ3sot8hRGdY1SomzsPOlk",
   authDomain: "excel-gam-zon.firebaseapp.com",
-  databaseURL: "https://excel-gam-zon.firebaseio.com",
   projectId: "excel-gam-zon",
   storageBucket: "excel-gam-zon.firebasestorage.app",
   messagingSenderId: "849616610846",
@@ -14,14 +12,6 @@ const firebaseConfig = {
   measurementId: "G-8Q6W427J8R"
 };
 
-// Initialize Singleton Instances
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-
-// Exporting the "Hearts" of the system
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Helper to handle the "Silent Entrance"
-export const loginAnonymously = () => signInAnonymously(auth);
-
-export default app;
